4e6c8c6a91a0da1fcf038b8cd1c886321a4e34e6
XiaoqiTang <xiaoqit@design.upenn.edu>
2019-04-08 00:27:59 -0400

    HW03-Xiaoqi Tang

---
+++ data/restaurantData.js
@@ -0,0 +1,3 @@
+var restaurantData = [{"lng":39.9553,"lat":-75.1975,"label":"LaTao Hot Pot"},
+{"lng":39.9548,"lat":-75.1939,"label":"SangKee Noodle House"},
+{"lng":39.9550,"lat":-75.2025,"label":"U-Town"}]
--- lab/lab2/part1.js
+++ lab/lab2/part1.js
@@ -3,7 +3,9 @@
 
 ## Introduction
 
-Set variables "query1" through "query8" by using an underscore function to answer the specified question. When you are finished, check your browser's console log to test the results.
+Set variables "query1" through "query8" by using an underscore function to 
+answer the specified question. 
+When you are finished, check your browser's console log to test the results.
 
 For data, use the following two lists of board game collections.
 ===================== */
@@ -19,7 +21,7 @@ What is the first game in Ross's list?
 ===================== */
 
 var query1;
-
+query1=_.first(rossGameList,1)[0];
 console.log('What is the first game in Ross\'s list?', query1);
 
 /* =====================
@@ -27,6 +29,7 @@ What are all of the games except for the first game in ross's list?
 ===================== */
 
 var query2;
+query2=_.last(rossGameList,rossGameList.length-1).join();
 
 console.log('What are all of the games except for the first game in Ross\'s list?', query2);
 
@@ -35,7 +38,7 @@ What is the last game in Nathan's list?
 ===================== */
 
 var query3;
-
+query3=_.last(nathanGameList,1)[0];
 console.log('What is the last game in Nathan\'s list?', query3);
 
 /* =====================
@@ -43,7 +46,7 @@ What are all of the games in Nathan's list except for the last?
 ===================== */
 
 var query4;
-
+query4=_.first(nathanGameList,nathanGameList.length-1).join();
 console.log('What are all of the games in Nathan\'s list except for the last?', query4);
 
 /* =====================
@@ -51,15 +54,16 @@ What would Nathan's game list look like if he sold "catan"?
 ===================== */
 
 var query5;
-
+query5=_.without(nathanGameList,"catan").join();
 console.log('What would Nathan\'s game list look like if he sold "catan"?', query5);
 
 /* =====================
-If Nathan and Ross play a board game, what are their options? This should be a list of all games owned by ross or Nathan, with no duplicates.
+If Nathan and Ross play a board game, what are their options? This should be a list of 
+all games owned by ross or Nathan, with no duplicates.
 ===================== */
 
 var query6;
-
+query6=_.uniq(_.union(rossGameList, nathanGameList)).join();
 console.log('If Nathan and Ross play a board game, what are their options? This should be a list of all games owned by ross or Nathan, with no duplicates.', query6);
 
 /* =====================
@@ -67,7 +71,7 @@ Which games are owned by both Ross and Nathan?
 ===================== */
 
 var query7;
-
+query7=_.uniq(_.intersection(rossGameList, nathanGameList)).join();
 console.log('Which games are owned by both Ross and Nathan', query7);
 
 /* =====================
@@ -75,5 +79,5 @@ Which games are exclusive to collections? In other words, only owned by either R
 ===================== */
 
 var query8;
-
+query8=_.difference(_.uniq(_.union(rossGameList, nathanGameList)),_.uniq(_.intersection(rossGameList, nathanGameList))).join();
 console.log('Which games are exclusive to one collection? In other words, only owned by either Ross or Nathan (but not both!).', query8);
